6cc29c51a43d1d7681f9ac6f1e84855be0ff0733
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-10-14 15:31:30 -0300

    Little refactoring. Added documentation inline

--- src/Koine.WebComponents.FancySelect.js
+++ src/Koine.WebComponents.FancySelect.js
@@ -3,6 +3,14 @@ var exports = exports || undefined;
 (function (Koine) {
   "use strinct";
 
+  /**
+   * Fancy Select
+   * @param jQuery $source Jquery object for the source select
+   * @param jQuery $destination Jquery object for the destination select
+   * @param jQuery $container Jquery object for the container in which the
+   * @param function the jquery function
+   *  fancy select must be rendered
+   */
   var FancySelect = function ($source, $destination, $container, $) {
     var self, source, destination;
     this.source      = new Koine.Decorators.Dom.SelectDecorator($source[0]);
@@ -26,64 +34,63 @@ var exports = exports || undefined;
 
     $container.on('click', '[data-add-selected]', function (e) {
       e.preventDefault();
-      self.transferSelected(source, destination, $container.find('.source .option.selected'));
+      self._transferSelected(source, destination, $container.find('.source .option.selected'));
     });
 
     $container.on('click', '[data-add-all]', function (e) {
       e.preventDefault();
-      self.transferSelected(source, destination, $container.find('.source .option'));
+      self._transferSelected(source, destination, $container.find('.source .option'));
     });
 
     $container.on('click', '[data-remove-selected]', function (e) {
       e.preventDefault();
-      self.transferSelected(destination, source, $container.find('.destination .option.selected'));
+      self._transferSelected(destination, source, $container.find('.destination .option.selected'));
     });
 
     $container.on('click', '[data-remove-all]', function (e) {
       e.preventDefault();
-      self.transferSelected(destination, source, $container.find('.destination .option'));
+      self._transferSelected(destination, source, $container.find('.destination .option'));
     });
 
   };
 
   FancySelect.prototype = {
+
+    /**
+     * Get the source select decorator
+     * @return Koine.Decorators.Dom.SelectDecorator
+     */
     getSource: function () {
       return this.source;
     },
 
+    /**
+     * Get the destination select decorator
+     * @return Koine.Decorators.Dom.SelectDecorator
+     */
     getDestination: function () {
       return this.destination;
     },
 
-    setContainer: function (container) {
-      this.container = container;
+    /**
+     * Sets the container of the rendering
+     * @param jQuery $container the jQuery
+     * @return self
+     */
+    setContainer: function ($container) {
+      this.$container = $container;
     },
 
+    /**
+     * @return jQuery
+     */
     getContainer: function () {
-      return this.container;
+      return this.$container;
     },
 
-    transferSelected: function (origin, destination, selected) {
-      var values = [], options = [];
-
-      selected.each(function () {
-        values.push($(this).data('value'));
-      });
-
-      values.forEach(function (value) {
-        origin.getOptions().forEach(function (option) {
-          if (value == option.getValue()) {
-            options.push(option);
-          }
-        });
-      });
-
-      origin.removeOptions(options);
-      destination.addOptions(options);
-
-      this.render();
-    },
-
+    /**
+     * Append the proper html to the $container
+     */
     render: function () {
       var sourceOptions    = this._getOptionsHtml(this.getSource()),
         destinationOptions = this._getOptionsHtml(this.getDestination()),
@@ -107,6 +114,37 @@ var exports = exports || undefined;
       this.getContainer().html(html);
     },
 
+    /**
+     * Transfers elements from the origin select decorator to
+     * destination select decorator
+     *
+     * @param Koine.Decorators.Dom.SelectDecorator origin
+     * @param Koine.Decorators.Dom.SelectDecorator destination
+     * @param jQuery $selected
+     */
+    _transferSelected: function (origin, destination, $selected) {
+      var values = [], options = [];
+
+      $selected.each(function () {
+        values.push($(this).data('value'));
+      });
+
+      values.forEach(function (value) {
+        origin.getOptions().forEach(function (option) {
+          if (value == option.getValue()) {
+            options.push(option);
+          }
+        });
+      });
+
+      origin.removeOptions(options);
+      destination.addOptions(options);
+    },
+
+    /**
+     * Get the html for the options
+     * @return String
+     */
     _getOptionsHtml: function (select) {
       var html = [],
         template = this.template;
@@ -121,6 +159,10 @@ var exports = exports || undefined;
       return html.join('');
     },
 
+    /**
+     * Get the buttons html
+     * @return String
+     */
     _getButtonsHtml: function () {
       return [
         '<div class="actions">',
@@ -132,6 +174,12 @@ var exports = exports || undefined;
       ].join('');
     },
 
+    /**
+     * Acts as a template engine
+     * @param String string
+     * @param object vars
+     * @return String
+     */
     template: function (template, vars) {
       var search;
       vars = vars || {};
