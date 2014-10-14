1c85c1e7c1feec1636bd29a10f0ea11f1eb3190d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-10-14 16:33:33 -0300

    Updated dependency

--- examples/fancy_multiple_select/js.js
+++ examples/fancy_multiple_select/js.js
@@ -1,31 +1,13 @@
-var fancySelect, source, destination, container;
-var Koine = Koine || {};
+var fancySelect,
+  Koine = Koine || {};
 
-$(document).ready(function () {
-  container   = $("#fancy_select");
-  source      = $("#source");
-  destination = $("#destination");
-  fancySelect = new Koine.WebComponents.FancySelect(source, destination, container, $);
-
-  [
-    ['1', 'one'],
-    ['2', 'two'],
-    ['3', 'three'],
-    ['4', 'four'],
-  ].forEach(function (value) {
-    var source = fancySelect.getSource();
-    var option = source.createOption(value[0], value[1], true);
-    source.addOption(option);
-  });
-
-  [
-    ['5', 'five'],
-    ['6', 'six'],
-  ].forEach(function (value) {
-    var destination = fancySelect.getDestination();
-    var option = destination.createOption(value[0], value[1], true);
-    destination.addOption(option);
-  });
+(document).ready(function () {
+  fancySelect = new Koine.WebComponents.FancySelect(
+    $("#source"),
+    $("#destination"),
+    $("#fancy_select"),
+    jQuery
+  );
 
   fancySelect.render();
 });
